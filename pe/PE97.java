bb5ac625c1f555a6824f87c2e2d927bb6779ce0d
Jonathan Lam <jlam55555@gmail.com>
2020-02-01 00:52:28 -0500

    implemented repeated squaring exponent pe97

--- pe/PE97.java
+++ pe/PE97.java
@@ -9,9 +9,8 @@ public class PE97 extends PERunnable<Long> {
         BigInteger exp = BigInteger.valueOf(7830457),
                 mod = BigInteger.valueOf(10000000000L),
                 coef = BigInteger.valueOf(28433),
-                one = BigInteger.ONE,
                 two = BigInteger.TWO;
-        return two.modPow(exp, mod).multiply(coef).add(one).mod(mod).longValue();
+        return two.modPow(exp, mod).multiply(coef).mod(mod).longValue()+1;
     }
 
     private long method2() {
@@ -23,6 +22,23 @@ public class PE97 extends PERunnable<Long> {
         return n+1;
     }
 
+    // self-implemented modpow with repeated squaring, like method1
+    // if one less digit in mod, could implement with simply longs grrr
+    // a little slower than using modpow
+    private long method3() {
+        BigInteger r, z, x, n;
+        for(r = BigInteger.valueOf(28433),
+            z = BigInteger.valueOf(10000000000L),
+            x = BigInteger.TWO,
+            n = BigInteger.valueOf(7830457);
+            n.signum()!=0; n = n.shiftRight(1)) {
+            if(n.testBit(0))
+                r = r.multiply(x).mod(z);
+            x = x.multiply(x).mod(z);
+        }
+        return r.longValue()+1;
+    }
+
     @Override
     public Long run(int version) {
         switch(version) {
@@ -30,6 +46,8 @@ public class PE97 extends PERunnable<Long> {
             return method1();
         case 1:
             return method2();
+        case 2:
+            return method3();
         }
         return -1L;
     }
